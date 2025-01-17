b5e3b11d6449b2ced12db8448f5ca97ac412efb8
mgarber21 <53231272+mgarber21@users.noreply.github.com>
2025-01-16 17:54:08 -0800

    uploading package contents

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, find_packages
 
-# Dynamically read requirements.txt
-with open("requirements.txt", "r") as f:
-    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
-
 setup(
     name="WonderPeaks",
-    version="0.1.0",
+    version="0.1.6",
+    description="Peak and UTR calling functions for ChIP-seq and RNA-seq in Fungi with WonderPeaks",
+    author="Megan E. Garber",
+    author_email="mgarber21@gmail.com",
+    url="https://github.com/mgarber21/WonderPeaks",
+    license="MIT",
     packages=find_packages(),
-    install_requires=requirements,  # Dependencies are loaded dynamically
+    include_package_data=True,  # Enables MANIFEST.in
+    install_requires=[],  # no dependencies
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
